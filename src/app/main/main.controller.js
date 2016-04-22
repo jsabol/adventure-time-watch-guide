e1826badf7b35d8b559785dc5156a57524e0af2b
Juliet Sabol <julietsabol@gmail.com>
2016-04-22 11:41:25 +0700

    Basic functionality: list episodes, select tier

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,34 +6,79 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($log, $http) {
     var vm = this;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1461165430328;
-    vm.showToastr = showToastr;
+    vm.episodes = [];
+    vm.currentTier = 1;
 
-    activate();
+    //temporary
+    vm.totalSeasons = 6;
+    vm.totalEpisodes = 199;
 
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
+    getEpisodes();
+
+    vm.isInTier = function(value){
+        return value.tier <= vm.currentTier;
+    };
+
+    vm.showSeasonHeader = function(ep, $index, arr){
+      return $index == 0 || (ep.season != arr[$index-1].season);
     }
 
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
+    function getEpisodes(){
+      $http.get('https://spreadsheets.google.com/feeds/list/14ciO3hpZXigTlBWdi0aCyxOXeSKEAdlb1BPBpCKAqKk/2/public/values?alt=json')
+        .then(function success(response){
+          var rows = response.data.feed.entry;
+          vm.episodes = rows.map(processEpisodeData);
+        }, function error(){
+
+        });
     }
 
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
 
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
+    function processEpisodeData(ep){
+
+      //create arc array
+      var arcs = [];
+      if(ep.gsx$arc.$t){
+        arcs.push(ep.gsx$arc.$t);
+      }
+      if(ep.gsx$arc2.$t){
+        arcs.push(ep.gsx$arc2.$t);
+      }
+      if(ep.gsx$arc3.$t){
+        arcs.push(ep.gsx$arc3.$t);
+      }
+
+      return {
+        season: parseInt(ep.gsx$season.$t),
+        number: parseInt(ep.gsx$number.$t),
+        title: ep.gsx$title.$t,
+        arcs: arcs,
+        article: ep.gsx$article.$t,
+        tier: parseInt(ep.gsx$priority.$t)
+      };
+
+    }//processEpisodeData
+
   }
 })();
+
+angular.module('adventureTimeWatchGuide')
+  .filter('minutesToTime', [function() {
+    return function(minutes) {
+        return new Date(1970, 0, 1).setSeconds(minutes*60);
+    };
+}])
+
+
+angular.module('adventureTimeWatchGuide')
+  .filter('episodesToTime', [function() {
+    return function(episodes) {
+      var totalMinutes = episodes * 11;
+      var hours = totalMinutes / 60;
+      var mins = totalMinutes % 60;
+      return Math.floor(hours) + ' hr '+ mins + ' min';
+    };
+}]);
